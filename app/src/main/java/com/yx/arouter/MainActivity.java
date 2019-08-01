cac4dd31f4c9f3210d86b6325d82f483b0dca6ac
yanxun <1768695841@qq.com>
2019-08-01 16:29:42 +0800

    1234

--- app/src/main/java/com/yx/arouter/MainActivity.java
+++ app/src/main/java/com/yx/arouter/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private int a;
+    private int a = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

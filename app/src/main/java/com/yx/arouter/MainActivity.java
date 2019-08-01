8f3b884d0ecc02d1449b33fd04c0e9921302740b
yanxun <1768695841@qq.com>
2019-08-01 16:16:07 +0800

    fsfgrdsfdg

--- app/src/main/java/com/yx/arouter/MainActivity.java
+++ app/src/main/java/com/yx/arouter/MainActivity.java
@@ -4,10 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
     }
